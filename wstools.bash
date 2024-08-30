38b0eaeabdfb88351dace5c220e35e42e3ef0545
George <gflanagin@richmond.edu>
2024-08-30 15:19:56 -0400

    Updated.

--- wstools.bash
+++ wstools.bash
@@ -47,8 +47,7 @@ function all_users_to_group
     done
 }
 
-export my_computers="adam alexis boyi camryn cooper erica evan hamilton justin kevin mayer michael sarah thais "
-export linux8="adam alexis boyi camryn erica evan irene justin kevin michael sarah"
+export my_computers="adam alexis boyi camryn cooper erica evan justin kevin mayer michael thais "
 
 # echo '$my_computers' is set to "$my_computers"
 # echo '$all_users' is set to "$all_users"
